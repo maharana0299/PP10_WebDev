08d7f5755963085a9a3f89759e26bc51fcea2536
Nirbhay <nirbhay0299@gmail.com>
2021-06-02 01:49:56 +0530

    Hackerrank activity done

--- automation/hackerRankActivity/ayncAndAwait.js
+++ automation/hackerRankActivity/ayncAndAwait.js
@@ -9,7 +9,7 @@ async function hack() {
             headless: false,
             defaultViewport: null,
             args: ['--start-maximized'],
-            slowMo: 50, // to slow things up by x millis
+            // slowMo: 50, // to slow things up by x millis
         })
 
         let pages = await (browser).pages();
@@ -39,34 +39,23 @@ async function hack() {
         ]);
 
         // get all 4 questions 
+
+        await gpage.waitForSelector('h4');
+
+        // let questions = await getQuestions(gpage);
         let questions = await getQuestions(gpage);
-        console.log(questions.length);
+        console.log(questions);
+
+        for (let i = 0; i < questions.length; i++) {
+            // now we have link, all we have to do is to submimt the code 
+
+            let newPage = await browser.newPage();
+            await newPage.goto(questions[i]);
+            await doSubmmissionStufss(newPage);
+            await newPage.close();
+        }
         // clock first question
         // await gpage.waitForSelector('.ui-btn.ui-btn-normal.primary-cta.ui-btn-primary.ui-btn-styled'); // wait uniti this element comes/load
-
-        // await Promise.all([
-        //     gpage.waitForNavigation(),
-        //     gpage.click('.ui-btn.ui-btn-normal.primary-cta.ui-btn-primary.ui-btn-styled')
-        // ]);
-
-        // await gpage.waitForSelector('[data-attr2="Editorial"]'); // wait uniti this element comes/load
-
-        // // editorial 
-        // await Promise.all([
-        //     gpage.waitForNavigation(),
-        //     gpage.click('[data-attr2="Editorial"]')
-        // ]);
-
-        // await handleLockBtn('.ui-btn.ui-btn-normal.ui-btn-primary.ui-btn-styled', gpage);
-
-        // // returning code and language 
-        // let codeInfo = await getCodes(gpage);
-
-        // await Promise.all([gpage.waitForNavigation(),
-        //     gpage.click('[data-attr2="Problem"]')
-        // ]);
-
-        // await pasteCodeAndSubmit(gpage, codeInfo);
 
 
     } catch (err) {
@@ -76,12 +65,38 @@ async function hack() {
 
 hack();
 
+async function doSubmmissionStufss(gpage) {
+
+    // await Promise.all([
+    //     gpage.waitForNavigation(),
+    //     gpage.click('.ui-btn.ui-btn-normal.primary-cta.ui-btn-primary.ui-btn-styled')
+    // ]);
+
+    await gpage.waitForSelector('[data-attr2="Editorial"]'); // wait uniti this element comes/load
+
+    // editorial 
+    await Promise.all([
+        gpage.waitForNavigation(),
+        gpage.click('[data-attr2="Editorial"]')
+    ]);
+
+    await gpage.waitForTimeout(3000);
+    await handleLockBtn('.ui-btn.ui-btn-normal.ui-btn-primary.ui-btn-styled', gpage);
+
+    // returning code and language 
+    let codeInfo = await getCodes(gpage);
+
+    await Promise.all([gpage.waitForNavigation(),
+        gpage.click('[data-attr2="Problem"]')
+    ]);
+
+    await pasteCodeAndSubmit(gpage, codeInfo);
+}
+
 function getQuestions(gpage) {
-    return gpage.evaluate(() => {
-        let a = document.querySelectorAll('h4');
-        console.log(a);
-        return a;
-    })
+    return gpage.evaluate(() =>
+        Array.from(document.querySelectorAll('.js-track-click.challenge-list-item'),
+            e => e.href));
 }
 async function pasteCodeAndSubmit(gpage, codeInfo) {
 
@@ -109,10 +124,10 @@ async function pasteCodeAndSubmit(gpage, codeInfo) {
     await gpage.keyboard.press("KeyV");
     await gpage.keyboard.up("Control");
 
-    // await Promise.all(
-    //     gpage.waitForNavigation(10000),
-    //     gpage.click(".ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled")
-    // );
+    await Promise.all([
+        // gpage.waitForNavigation(10000),
+        gpage.click(".ui-btn.ui-btn-normal.ui-btn-primary.pull-right.hr-monaco-submit.ui-btn-styled")
+    ]);
 
     await gpage.goBack();
 }
