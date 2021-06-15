0f3ebe3bc3b4300e4306d11bda9bbe12c90e8cde
Nirbhay <nirbhay0299@gmail.com>
2021-06-15 20:06:01 +0530

    .

--- automation/SomePractice/bankData.js
+++ automation/SomePractice/bankData.js
@@ -7,7 +7,7 @@ const puppeteer = require('puppeteer');
 async function bandData(url) {
     try {
         const browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             slowMo: 50, // for slowing a bit 
             defaultViewport: null, // null the default viewport 
             args: ["--start-maximized"], // for full screen
@@ -30,15 +30,45 @@ async function bandData(url) {
         // now extract data
 
         let maxSpent = await page.evaluate(() => {
+            let allTars = document.querySelectorAll('tbody tr');
+            let allAmounts = [];
+            let allDescriptions = [];
 
+            for (let i = 0; i < allTars.length; i++) {
+                let allTd = allTars[i].querySelectorAll('td');
+                allAmounts.push(allTd[allTd.length - 1].innerText);
+
+                allDescriptions.push(allTd[2].innerText);
+            }
+
+            for (let i = 0; i < allAmounts.length; i++) {
+                let a = allAmounts[i].split(" ");
+                let b = a[1].split(",");
+                a[1] = b.join("");
+                allAmounts[i] = Number(a[0] + a[1]);
+            }
+
+            let maxSpent = 0;
+            let index = -1;
+            for (let i = 0; i < allAmounts.length; i++) {
+
+                if (allAmounts[i] < maxSpent) {
+                    maxSpent = allAmounts[i];
+                    index = i;
+                }
+            }
+
+            return "We Have Spent " + (-maxSpent) + " on " + allDescriptions[index];
         });
 
         console.log(maxSpent);
+
+        await browser.close();
     } catch (e) {
         console.log(e);
     }
 
-    // await browser.close();
+
 }
 
 let url = 'https://demo.applitools.com';
