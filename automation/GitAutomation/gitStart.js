44e45846aa4fe20a7c909762257580c25cd1daa0
Nirbhay <nirbhay0299@gmail.com>
2021-06-18 00:36:41 +0530

    .

--- automation/GitAutomation/gitStart.js
+++ automation/GitAutomation/gitStart.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 const fs = require('fs');
 let data = {};
 
-let browser;
+
 
 async function automation(githubLink) {
     try {
@@ -25,78 +25,79 @@ async function automation(githubLink) {
             data[allTopics[i].title] = {};
         }
         for (let i = 0; i < allTopics.length; i++) {
-            await processProjects(allTopics[i].title, allTopics[i].url);
+            await processProjects(browser, allTopics[i].title, allTopics[i].url);
         }
         console.log(data);
         fs.writeFileSync('gitRep.json', JSON.stringify(data));
-        async function processProjects(topic, projectUrl) {
 
-            let newPage = await browser.newPage();
-            await newPage.goto(projectUrl);
-
-            await newPage.waitForSelector('.lh-condensed a.text-bold');
-            console.log('Working On Projects....');
-            let allProjects = await newPage.evaluate(() => {
-                let topics = document.querySelectorAll('.lh-condensed a.text-bold');
-                let allProjects = [];
-                let length = topics.length > 5 ? 5 : 0;
-                for (let i = 0; i < length; i++) {
-                    let projectUrl = 'https://github.com' + topics[i].getAttribute('href');
-                    let pname = topics[i].innerText;
-                    allProjects.push({ url: projectUrl, pname: pname });
-                }
-
-                return allProjects;
-            });
-
-            // console.log(allProjects);
-
-            // now we have all Projects
-            for (let i = 0; i < allProjects.length; i++) {
-                data[topic][allProjects[i].pname] = { url: allProjects[i].url, issues: [] };
-            }
-
-            // now work for all issues 
-            for (let i = 0; i < allProjects.length; i++) {
-
-                // get 3 issues 
-                let issueUrl = allProjects[i].url + '/issues';
-                await addIssuesToData(newPage, topic, allProjects[i].pname, issueUrl);
-            }
-
-            await newPage.close();
-        }
-
-        async function addIssuesToData(newPage, topic, project, issueUrl) {
-            await newPage.goto(issueUrl);
-            console.log('Adding Issue');
-            try {
-                await newPage.waitForSelector('.h4.markdown-title');
-                let issues = await newPage.evaluate(() => {
-                    let allIssues = document.querySelectorAll('.h4.markdown-title');
-                    let issues = [];
-                    let length = allIssues.length > 5 ? 5 : allIssues.length;
-                    for (let i = 0; i < length; i++) {
-                        issues.push({
-                            issue: allIssues[i].innerText,
-                            url: 'https://github.com' + allIssues[i].getAttribute('href'),
-                        });
-                    }
-
-                    return issues;
-                })
-
-                data[topic][project].issues = issues;
-            } catch (e) {
-                console.log(e);
-            }
-            // console.log(issues);
-        }
+        await browser.close();
     } catch (e) {
         console.log(e);
     }
 }
 
+async function processProjects(browser, topic, projectUrl) {
+
+    let newPage = await browser.newPage();
+    await newPage.goto(projectUrl);
+
+    await newPage.waitForSelector('.lh-condensed a.text-bold');
+    console.log('Working On Projects....');
+    let allProjects = await newPage.evaluate(() => {
+        let topics = document.querySelectorAll('.lh-condensed a.text-bold');
+        let allProjects = [];
+        let length = topics.length > 5 ? 5 : 0;
+        for (let i = 0; i < length; i++) {
+            let projectUrl = 'https://github.com' + topics[i].getAttribute('href');
+            let pname = topics[i].innerText;
+            allProjects.push({ url: projectUrl, pname: pname });
+        }
+
+        return allProjects;
+    });
+
+    // console.log(allProjects);
+
+    // now we have all Projects
+    for (let i = 0; i < allProjects.length; i++) {
+        data[topic][allProjects[i].pname] = { url: allProjects[i].url, issues: [] };
+    }
+
+    // now work for all issues 
+    for (let i = 0; i < allProjects.length; i++) {
+
+        // get 3 issues 
+        let issueUrl = allProjects[i].url + '/issues';
+        await addIssuesToData(newPage, topic, allProjects[i].pname, issueUrl);
+    }
+
+    await newPage.close();
+}
+
+async function addIssuesToData(newPage, topic, project, issueUrl) {
+    await newPage.goto(issueUrl);
+    console.log('Adding Issue');
+    try {
+        await newPage.waitForSelector('.h4.markdown-title');
+        let issues = await newPage.evaluate(() => {
+            let allIssues = document.querySelectorAll('.h4.markdown-title');
+            let issues = [];
+            let length = allIssues.length > 5 ? 5 : allIssues.length;
+            for (let i = 0; i < length; i++) {
+                issues.push({
+                    issue: allIssues[i].innerText,
+                    url: 'https://github.com' + allIssues[i].getAttribute('href'),
+                });
+            }
+
+            return issues;
+        })
+
+        data[topic][project].issues = issues;
+    } catch (e) {
+        console.log(e);
+    }
+}
 async function getTopics(page) {
 
     console.log('Getting Topics....');
